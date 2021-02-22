f135891813a0f2fe50fb2c3775755afbe0ff21ac
nigranac <can.argin.16@gmail.com>
2021-02-22 11:47:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const quote = document.getElementById("quote");
+
+function getKanye() {
+  fetch("https://api.kanye.rest")
+    .then((resp) => resp.json())
+    .then(function (data) {
+      document.getElementById("quote").innerHTML = data.quote;
+
+      const tweet = encodeURIComponent(
+        `"${data.quote}"-@kanyewest via https://kanye.rest @ajzbc`
+      );
+      document.getElementById(
+        "tweet"
+      ).href = `https://twitter.com/intent/tweet?text=${tweet}`;
+    });
+}
